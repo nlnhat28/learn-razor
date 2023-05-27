9cf4370fa5bac9ee8069a4f6f830e651fddf09af
nlnhat28 <nl.nhat28@gmail.com>
2023-05-27 21:08:51 +0700

    Customize management pages

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
@@ -5,6 +7,13 @@ namespace RAZOR_EF.Models
 {
     public class AppUser : IdentityUser
     {
+        [StringLength(255)]
+        [DisplayName("Full name")]
+        [DataType(DataType.Text)]
+        public string? FullName {get; set;}
 
+        [DisplayName("Birthday")]
+        [DataType(DataType.Date)]
+        public DateTime? Birthday { get; set; }
     }
 }
---
+++ Validations/Unique.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using RAZOR_EF.Models;
+
+namespace RAZOR_EF.Validations
+{
+    public class Unique : ValidationAttribute
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public Unique(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            ErrorMessage = "{0} is already taken.";
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                string userName = value.ToString();
+
+                var _user = _userManager.FindByNameAsync(userName).GetAwaiter().GetResult();
+                if (_user != null) //Username is already taken.
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
